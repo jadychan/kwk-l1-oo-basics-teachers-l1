cf6650588318d798131078ab4b5e8f8670665b16
jadychan <jadychan123@gmail.com>
2018-06-07 06:37:40 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
 # book.rb
 # Add you Book class here
+class Book
+
+  def initialize(title)
+    @title = title
+  end
+
+  #getter for title
+  def title
+    @title
+  end
+
+  def author=(author)
+    @author = author
+  end
+end
+
+Book.new("And Then There Were None")
